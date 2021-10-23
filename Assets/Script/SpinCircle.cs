9140f896e5e65ef38d6a1e1df2c7b4809012bea2
nogu06 <knocktokotoko@gmail.com>
2021-10-23 17:24:16 +0900

    仮のモバイル用戦闘シーンを作り始めた

---
+++ Assets/Script/SpinCircle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinCircle : MonoBehaviour
+{
+    private Vector3 m_pos;
+    float startTime = 0.0f;
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,0,30));
+    }
+}
