67e5f5a7f6eb22c14d34c407eafcff8970563728
nogu06 <knocktokotoko@gmail.com>
2021-10-30 18:25:45 +0900

    回転目押しシステムの基礎を作った

--- Assets/Script/SpinCircle.cs
+++ Assets/Script/SpinCircle.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpinCircle : MonoBehaviour
 {
-    private Vector3 m_pos;
-    float startTime = 0.0f;
+    private Vector3 pos;
+    public float speed = 1.0f;
+    [SerializeField] Image image;
+    [SerializeField] MonsterBase monster;
+    public MonsterBase Monster { get => monster; private set => monster = value; }
+
+    [SerializeField] float circleLength = 45.0f;
+    public float CircleLength { get => circleLength; private set => circleLength = value; }
 
     private void Start()
     {
+        Application.targetFrameRate = 60;
+        this.image.fillAmount = CircleLength / 360;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0,0,30));
+        transform.Rotate(new Vector3(0, 0, 0.1f * speed));
+
     }
 }
