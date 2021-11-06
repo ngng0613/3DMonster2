ae47d28e04f7d113269f41496845926cda836845
nogu06 <knocktokotoko@gmail.com>
2021-11-06 20:29:50 +0900

    若干の変更

--- Assets/Script/Mobile/MobileBattle.cs
+++ Assets/Script/Mobile/MobileBattle.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MobileBattle : MonoBehaviour
 {
     [SerializeField] List<SpinCircle> circleList = new List<SpinCircle>();
     [SerializeField] int plusCount = 1;
+
+    private float criticalPower = 0;
+    [SerializeField] Image criticalGauge;
+    [SerializeField] TextMeshProUGUI critGaugeText;
+
+    [SerializeField] MonsterBase tempMonster;
+
+    private void Start()
+    {
+        tempMonster.SetCurrentHPValue(tempMonster.GetMaxHPValue());
+    }
 
     public void OnClick()
     {
@@ -17,13 +30,26 @@ public class MobileBattle : MonoBehaviour
             {
                 Debug.Log(circle.Monster.GetNickname() + " : " + angleDifference);
                 circle.Monster.coolTime += plusCount;
+                tempMonster.TakeDamage(5);
                 test = true;
             }
         }
-
         if (test == false)
         {
             Debug.Log("失敗…");
+            if (criticalPower > 0)
+            {
+                criticalPower -= 5;
+            }
+        }
+        else
+        {
+            if (criticalPower < 100)
+            {
+                criticalPower += 5;
+                criticalGauge.transform.localScale = new Vector3(1.0f, criticalPower / 100, 1.0f);
+                critGaugeText.text = "必殺\n" + criticalPower;
+            }
         }
 
     }
