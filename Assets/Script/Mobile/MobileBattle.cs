c408f93d7a5958b82d50b876d2ab1e8a3abaee2b
nogu06 <knocktokotoko@gmail.com>
2021-11-06 15:51:48 +0900

    モバイルバトルのスクリプト

---
+++ Assets/Script/Mobile/MobileBattle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileBattle : MonoBehaviour
+{
+    [SerializeField] List<SpinCircle> circleList = new List<SpinCircle>();
+    [SerializeField] int plusCount = 1;
+
+    public void OnClick()
+    {
+        bool test = false;
+        foreach (SpinCircle circle in circleList)
+        {
+            float angleDifference = 0 + circle.transform.localEulerAngles.z;
+            if (circle.CircleLength > angleDifference)
+            {
+                Debug.Log(circle.Monster.GetNickname() + " : " + angleDifference);
+                circle.Monster.coolTime += plusCount;
+                test = true;
+            }
+        }
+
+        if (test == false)
+        {
+            Debug.Log("失敗…");
+        }
+
+    }
+}
