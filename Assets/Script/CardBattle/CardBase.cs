f5c0ae75b7cb4123a766463ecc02573120a5b5d2
nogu06 <knocktokotoko@gmail.com>
2021-11-13 20:12:28 +0900

    カードシステムの仮実装

---
+++ Assets/Script/CardBattle/CardBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardBase : MonoBehaviour
+{
+    public string cardName;
+    public Sprite mainImage;
+    public int cost;
+    public int power;
+}
---
+++ Assets/Script/CardBattle/CardData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Card", menuName = "ScriptableObjects/Card", order = 1)]
+public class CardData : ScriptableObject
+{
+    public string cardName;
+    public Sprite mainImage;
+    public int cost;
+    public int power;
+}
