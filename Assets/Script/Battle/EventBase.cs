16468254d427d15a821d3023493940438bcae486
nogu06 <knocktokotoko@gmail.com>
2021-11-27 19:08:22 +0900

    イベントをunityEventで作った

---
+++ Assets/Script/Battle/EventBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventBase : MonoBehaviour
+{
+    public UnityEvent eventTask = new UnityEvent();
+
+    public void A()
+    {
+        
+    }
+    
+}
