3e3eed09c52112b689d078c6936d884ea1ceae4a
nogu06 <knocktokotoko@gmail.com>
2021-12-09 23:51:10 +0900

    カードを離した時の処理を追加

--- Assets/Script/CardBattle/CardObject.cs
+++ Assets/Script/CardBattle/CardObject.cs
@@ -9,8 +9,20 @@ public class CardObject : MonoBehaviour
 {
     [SerializeField] CardData cardData;
     [SerializeField] Canvas canvas;
-    TextMeshProUGUI nameText;
-    Image image;
+    [SerializeField]TextMeshProUGUI nameText;
+    [SerializeField]Image image;
+
+    Vector3 relativePos;
+    Vector3 firstMousePos;
+
+    public delegate void Func();
+    Func handUpdateDelegate;
+        
+    public void Setup(Func func)
+    {
+        handUpdateDelegate = func;
+        UpdateText();
+    }
 
     private void UpdateText()
     {
@@ -20,12 +32,36 @@ public class CardObject : MonoBehaviour
 
     public void OnPointerEnter()
     {
-        this.gameObject.transform.DOLocalMoveY(64, 0.2f);
+        this.gameObject.transform.DOLocalMoveY(156, 0.2f);
+        this.gameObject.transform.DOScale(new Vector3(1.2f, 1.2f, 1.2f), 0.2f);
         canvas.sortingOrder = 1;
     }
     public void OnPointerExit()
     {
         this.gameObject.transform.DOLocalMoveY(0, 0.2f);
+        this.gameObject.transform.DOScale(Vector3.one, 0.2f);
+        canvas.sortingOrder = 0;
+    }
+
+    public void OnMouseDown()
+    {
+        relativePos = this.gameObject.transform.position;
+        firstMousePos = Input.mousePosition;
+        Debug.Log($"relative = {relativePos},  firstPos = {firstMousePos}");
+    }
+
+    public void OnMouseDrag()
+    {
+        this.gameObject.transform.position = relativePos + Input.mousePosition - firstMousePos;
+    }
+
+    public void OnMouseUp()
+    {
+        handUpdateDelegate.Invoke();
+    }
+
+    public void ResetSortingOrder()
+    {
         canvas.sortingOrder = 0;
     }
 }
--- Assets/Script/CardBattle/Hand.cs
+++ Assets/Script/CardBattle/Hand.cs
@@ -7,22 +7,33 @@ public class Hand : MonoBehaviour
     /// <summary>
     /// 手札
     /// </summary>
-    List<CardData> cardList;
+    [SerializeField] List<CardObject> cardList = new List<CardObject>();
+
+    [SerializeField] CardObject debugCard;
 
     /// <summary>
     /// 手札の追加
     /// </summary>
-    void AddHand(CardData card)
+    public void AddHand(CardObject card)
     {
         cardList.Add(card);
 
     }
 
+    public void AddHandDebug()
+    {
+        CardObject newCard = Instantiate(debugCard, this.gameObject.transform);
+        newCard.Setup(HandUpdate);
+        cardList.Add(newCard);
+        
+        HandUpdate();
+    }
     void HandUpdate()
     {
         for (int i = 0; i < cardList.Count; i++)
         {
-            
+            cardList[i].ResetSortingOrder();
+            cardList[i].transform.localPosition = new Vector2(i * 256, 0);
         }
     }
 }
