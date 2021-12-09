e854a8227d07254e6a03f904403d641571c70b84
nogu06 <knocktokotoko@gmail.com>
2021-12-09 15:55:04 +0900

    カードに触れたときの処理を追加

--- Assets/Script/CardBattle/CardObject.cs
+++ Assets/Script/CardBattle/CardObject.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using DG.Tweening;
 
 public class CardObject : MonoBehaviour
 {
     [SerializeField] CardData cardData;
+    [SerializeField] Canvas canvas;
     TextMeshProUGUI nameText;
     Image image;
 
@@ -14,5 +16,16 @@ public class CardObject : MonoBehaviour
     {
         nameText.text = cardData.cardName;
         image.sprite = cardData.mainImage;
+    }
+
+    public void OnPointerEnter()
+    {
+        this.gameObject.transform.DOLocalMoveY(64, 0.2f);
+        canvas.sortingOrder = 1;
+    }
+    public void OnPointerExit()
+    {
+        this.gameObject.transform.DOLocalMoveY(0, 0.2f);
+        canvas.sortingOrder = 0;
     }
 }
