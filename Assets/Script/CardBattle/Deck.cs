3c4483a78db6196525c7a274c4249cb295e04617
nogu06 <knocktokotoko@gmail.com>
2021-12-11 19:45:52 +0900

    デッキクラスを追加

---
+++ Assets/Script/CardBattle/Deck.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    List<CardData> cardList = new List<CardData>();
+
+    /// <summary>
+    /// デッキの初期設定
+    /// </summary>
+    /// <param name="cardList"></param>
+    public void Setup(List<CardData> cardList)
+    {
+        this.cardList = cardList;
+        Shuffle();
+    }
+
+    /// <summary>
+    /// デッキの中身をシャッフル
+    /// </summary>
+    public void Shuffle()
+    {
+        
+    }
+
+    /// <summary>
+    /// 特定のカードをサーチする
+    /// </summary>
+    public void Search()
+    {
+        
+    }
+}
