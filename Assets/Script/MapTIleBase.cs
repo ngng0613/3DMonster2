14b9185c8cac3b5b43675f6640e8034327830e67
nogu06 <knocktokotoko@gmail.com>
2022-01-15 19:57:13 +0900

    クラスの変更

--- Assets/Script/MapTIleBase.cs
+++ Assets/Script/MapTIleBase.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MapTIleBase : MonoBehaviour
+public class MapTileBase : MonoBehaviour
 {
     [SerializeField] Image _mainImage;
 }
