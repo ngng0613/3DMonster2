fa8ab9460f0e77c412d3a4ca34ad26b9441e90b7
nogu06 <knocktokotoko@gmail.com>
2022-01-17 12:43:53 +0900

    マップの移動処理を少し書いた

--- Assets/Script/MapContoroller.cs
+++ Assets/Script/MapContoroller.cs
@@ -6,14 +6,45 @@ using UnityEngine.Tilemaps;
 public class MapContoroller : MonoBehaviour
 {
     [SerializeField] GameObject _player;
-    [SerializeField] Tilemap _tileMap;
-
-    private void Update()
+    [SerializeField] float _playerMoveSpeed = 1.0f;
+    [SerializeField] MapEvent[] mapEventArray;
+    Vector3 _moveToPos;
+    private void Start()
     {
-        Vector3 mousePos = Input.mousePosition;
-        Vector3 tempPos = Camera.main.ScreenToWorldPoint(mousePos);
-        
+        for (int i = 0; i < mapEventArray.Length; i++)
+        {
+            mapEventArray[i].MovePlayer = MovePlayer;
+        }
 
     }
+
+    public void MovePlayer(Vector3 pos)
+    {
+        _moveToPos = pos;
+        StartCoroutine(MovePlayerCoroutine());
+    }
+
+    public IEnumerator MovePlayerCoroutine()
+    {
+        Vector3 startPos = _player.transform.position;
+        float distance = Vector3.Distance(_player.transform.position, _moveToPos);
+        float startTime = Time.time;
+   
+
+        while (true)
+        {
+            float t = (Time.time - startTime) / distance * _playerMoveSpeed;
+            Vector3 movePos = Vector3.Lerp(startPos, _moveToPos, t);
+            _player.transform.position = movePos;
+            if (t >= 1)
+            {
+                break;
+            }
+
+            yield return null;
+        }
+
+    }
+
 
 }
---
+++ Assets/Script/MapEvent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MapEvent : EventBase
+{
+    public delegate void MovePositionDelegate(Vector3 pos);
+    public MovePositionDelegate MovePlayer;
+    [SerializeField] bool _isSelect = false;
+    Player _player;
+    [SerializeField] Sprite _spriteSelected;
+    [SerializeField] Sprite _spriteUnselected;
+    [SerializeField] SpriteRenderer _spriteRenderer;
+
+    public void OnMouseOver()
+    {
+        _isSelect = true;
+        _spriteRenderer.sprite = _spriteSelected;
+    }
+
+    public void OnMouseDown()
+    {
+        MovePlayer.Invoke(this.transform.position);
+    }
+
+    public void OnMouseExit()
+    {
+        _isSelect = false;
+        _spriteRenderer.sprite = _spriteUnselected;
+     
+    }
+}
