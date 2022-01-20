d60995e3e4e28a11798366cca6a3bcb4f3ef0927
nogu06 <knocktokotoko@gmail.com>
2022-01-20 13:51:25 +0900

    更新

--- Assets/Script/CardBattle/ResultManager.cs
+++ Assets/Script/CardBattle/ResultManager.cs
@@ -1,8 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
 
 public class ResultManager : MonoBehaviour
 {
-    
+    [SerializeField] GameObject _resultTitle;
+    Sequence _sequence;
+    [SerializeField] Vector2 _titlePos;
+    [SerializeField] float _displaySpeed = 1.0f;
+
+    private void Start()
+    {
+        _sequence = DOTween.Sequence();
+        AnimationStart();
+    }
+    public void AnimationStart()
+    {
+        _sequence.Append(_resultTitle.transform.DOLocalMoveX(_titlePos.x,_displaySpeed));
+    }
+
 }
