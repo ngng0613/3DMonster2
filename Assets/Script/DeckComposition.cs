0090554842fc6abd0be7da54e68cbe2f9568cd5f
nogu06 <knocktokotoko@gmail.com>
2022-02-05 18:16:01 +0900

    デッキ編成のパネルをはめる挙動ができた

--- Assets/Script/DeckComposition.cs
+++ Assets/Script/DeckComposition.cs
@@ -1,30 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DeckComposition : MonoBehaviour
 {
     [SerializeField] PartOfMonsterList _partOfMonsterListPrefab;
     [SerializeField] GameObject _lane;
+    [SerializeField] float _spinSpeed = 3.0f;
 
-    [SerializeField] GameObject _partsPosition;
+    [SerializeField] GameObject[] _partsPosition;
 
     [SerializeField] CardObject[] _cardList1 = new CardObject[5];
     [SerializeField] CardObject[] _cardList2 = new CardObject[5];
     [SerializeField] CardObject[] _cardList3 = new CardObject[5];
 
-    private void Start()
+    PartOfMonsterList[] _setParts = new PartOfMonsterList[3];
+
+    public void Start()
     {
-        PartOfMonsterList part = Instantiate(_partOfMonsterListPrefab, _lane.transform);
-        part.SetMonsterSlot = SetMonsterSlot;
+
+        for (int i = 0; i < _partsPosition.Length; i++)
+        {
+            PartOfMonsterList part = Instantiate(_partOfMonsterListPrefab, _lane.transform);
+            part.transform.position = _partsPosition[i].transform.position;
+            part.BasePos = _partsPosition[i].transform.position;
+            part.Monster = GameManager.Instance.MonsterList[i];
+            part.DisplayUpdate();
+            part.SetMonsterSlot = SetMonsterSlot;
+        }
+
     }
 
-    public void SetMonsterSlot(MonsterBase monster, int slotId)
+    public void SetMonsterSlot(MonsterBase monster, int slotId, PartOfMonsterList part)
     {
-        StartCoroutine(SetMonsterAnimation(monster, slotId));
+        if (_setParts[slotId] != null)
+        {
+            _setParts[slotId].BackToBasePos();
+        }
+        _setParts[slotId] = part;
         switch (slotId)
         {
             case 1:
+           
                 Debug.Log($"スロット1に{monster.MonsterName}をセットしました");
                 break;
 
@@ -39,8 +57,11 @@ public class DeckComposition : MonoBehaviour
             default:
                 break;
         }
+        StartCoroutine(SetMonsterAnimation(monster, slotId));
+
 
     }
+
 
     public IEnumerator SetMonsterAnimation(MonsterBase monster, int slotId)
     {
@@ -73,17 +94,13 @@ public class DeckComposition : MonoBehaviour
             default:
                 break;
         }
-
-
-
     }
-
 
     public IEnumerator CardSpin(CardObject obj, CardData data)
     {
         while (true)
         {
-            obj.transform.localEulerAngles += new Vector3(0, 3, 0);
+            obj.transform.localEulerAngles += new Vector3(0, _spinSpeed, 0);
             if (obj.transform.localEulerAngles.y >= 90)
             {
                 obj.transform.localEulerAngles = new Vector3(0, 90, 0);
@@ -96,7 +113,7 @@ public class DeckComposition : MonoBehaviour
         obj.UpdateText();
         while (true)
         {
-            obj.transform.localEulerAngles -= new Vector3(0, 3, 0);
+            obj.transform.localEulerAngles -= new Vector3(0, _spinSpeed, 0);
             if (obj.transform.localEulerAngles.y >= 180)
             {
                 obj.transform.localEulerAngles = Vector3.zero;
--- Assets/Script/PartOfMonsterList.cs
+++ Assets/Script/PartOfMonsterList.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
+using DG.Tweening;
 
 public class PartOfMonsterList : MonoBehaviour, IPointerUpHandler
 {
-    public MonsterBase _monster;
+    public MonsterBase Monster;
     [SerializeField] string _slotTagName1 = "";
     [SerializeField] string _slotTagName2 = "";
     [SerializeField] string _slotTagName3 = "";
     [SerializeField] Image _image;
     [SerializeField] TextMeshProUGUI _nametext;
+    public Vector3 BasePos;
+    [SerializeField] float _moveSpeed = 1.0f;
 
-    public delegate void SetPanelDelegate(MonsterBase monster, int slotId);
+    public delegate void SetPanelDelegate(MonsterBase monster, int slotId, PartOfMonsterList partOfMonsterList);
     public SetPanelDelegate SetMonsterSlot;
+    bool isSet = false;
+
+    Sequence _sequence;
+
+    private void Start()
+    {
+        _sequence = DOTween.Sequence();
+    }
+
 
     public void Open()
     {
@@ -24,7 +36,13 @@ public class PartOfMonsterList : MonoBehaviour, IPointerUpHandler
 
     public void DisplayUpdate()
     {
-        _image.sprite = _monster.Image;
+        _image.sprite = Monster.Image;
+    }
+
+    public void OnClick()
+    {
+        _sequence.Pause();
+        _sequence.Kill();
     }
 
     public void OnDrag()
@@ -34,34 +52,52 @@ public class PartOfMonsterList : MonoBehaviour, IPointerUpHandler
 
     void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
     {
+
         var raycastResults = new List<RaycastResult>();
         EventSystem.current.RaycastAll(eventData, raycastResults);
-
+        isSet = false;
         foreach (var hit in raycastResults)
         {
+            Debug.Log(hit.gameObject.name);
             if (hit.gameObject.tag != null)
             {
                 if (hit.gameObject.CompareTag(_slotTagName1))
                 {
                     ///モンスタースロットにセットされた
                     this.gameObject.transform.position = hit.gameObject.transform.position;
-                    SetMonsterSlot.Invoke(_monster,1);
+                    SetMonsterSlot.Invoke(Monster, 1,this);
+                    isSet = true;
 
                 }
                 else if (hit.gameObject.CompareTag(_slotTagName2))
                 {
                     ///モンスタースロットにセットされた
                     this.gameObject.transform.position = hit.gameObject.transform.position;
-                    SetMonsterSlot.Invoke(_monster,2);
+                    SetMonsterSlot.Invoke(Monster, 2, this);
+                    isSet = true;
                 }
                 else if (hit.gameObject.CompareTag(_slotTagName3))
                 {
                     ///モンスタースロットにセットされた
                     this.gameObject.transform.position = hit.gameObject.transform.position;
-                    SetMonsterSlot.Invoke(_monster,3);
+                    SetMonsterSlot.Invoke(Monster, 3, this);
+                    isSet = true;
+                }
+                else if (hit.gameObject.CompareTag(this.gameObject.tag))
+                {
+                    hit.gameObject.GetComponent<PartOfMonsterList>().BackToBasePos();
                 }
             }
-
         }
+        if (isSet == false)
+        {
+            BackToBasePos();
+        }
+    }
+
+    public void BackToBasePos()
+    {
+        isSet = false;
+        _sequence.Append(this.gameObject.transform.DOMove(BasePos, 1.0f / _moveSpeed));
     }
 }
