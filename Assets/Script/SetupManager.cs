782a67c37eebcb2bbe29512ce7a2fb6cd51cc55d
nogu06 <knocktokotoko@gmail.com>
2022-02-12 11:19:54 +0900

    セットアップマネージャーの追加

---
+++ Assets/Script/SetupManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupManager : MonoBehaviour
+{
+    [SerializeField] List<MonsterBase> _monsterList;
+
+    void Awake()
+    {
+        GameManager.Instance.MonsterList = _monsterList;
+    }
+
+}
