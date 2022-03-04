1ba5ac005d64eb2b450eb27a46856418e9caaec8
nogu06 <knocktokotoko@gmail.com>
2022-03-04 23:52:36 +0900

    更新

---
+++ Assets/Script/GameManagerSub.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerSub : MonoBehaviour
+{
+    [SerializeField] List<MonsterBase> _monsterParty;
+    [SerializeField] List<int> _monsterInParty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _monsterParty = GameManager.Instance.MonsterParty;
+        _monsterInParty = GameManager.Instance.MonsterPartyIdList;
+    }
+}
