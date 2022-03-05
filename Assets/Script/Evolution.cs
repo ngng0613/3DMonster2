c1c5b907ce76da9916692c88732587d67db5dceb
nogu06 <knocktokotoko@gmail.com>
2022-03-05 13:18:08 +0900

    進化画面を二回開くと、前回の進化の表示が残ったままの点を修正

--- Assets/Script/Evolution.cs
+++ Assets/Script/Evolution.cs
@@ -30,6 +30,8 @@ public class Evolution : DeckManagerBase
         _parts = new PartOfMonsterList[6];
 
         this.gameObject.SetActive(true);
+        _evoCanvas.SetActive(false);
+        _evoMonsterObject.gameObject.SetActive(false);
 
         for (int i = 0; i < GameManager.Instance.MonsterList.Count; i++)
         {
