323ab277418ec101d9c8021e157f6f1830c85634
nogu06 <knocktokotoko@gmail.com>
2022-06-11 10:31:01 +0900

    チュートリアル機能のモック作成

---
+++ Assets/Script/TutorialManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField] List<GameManager> _tutorialObjectList;
+
+    public void StartTutorial()
+    {
+
+    }
+}
