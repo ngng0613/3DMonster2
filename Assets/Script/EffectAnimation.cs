94034a74a2871bc794cff8de4b6ce553231a8d03
nogu06 <knocktokotoko@gmail.com>
2022-06-25 18:04:34 +0900

    暫定的にアニメーション再生するようにした

--- Assets/Script/EffectAnimation.cs
+++ Assets/Script/EffectAnimation.cs
@@ -1,16 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EffectAnimation : MonoBehaviour
 {
+    [SerializeField] Image _animationView;
     [SerializeField] List<AnimationData> _animationList = new List<AnimationData>();
+
+    public void Start()
+    {
+        _animationView.color = Color.clear;
+    }
+
+    /// <summary>
+    /// アニメーションの再生
+    /// </summary>
+    /// <param name="playId">再生するアニメのID</param>
+    public void PlayAnimation(int playId)
+    {
+        if (playId >= _animationList.Count)
+        {
+            Debug.LogError($"指定されたPlayID{playId}はアニメーションリストの範囲外です");
+            return;
+        }
+
+        StartCoroutine(PlayAnimationImpl(_animationList[playId]));
+    }
+
+    IEnumerator PlayAnimationImpl(AnimationData animationData)
+    {
+        _animationView.color = Color.white;
+        for (int i = 0; i < animationData.SpriteAnimationArray.Length; i++)
+        {
+            _animationView.sprite = animationData.SpriteAnimationArray[i];
+            _animationView.SetNativeSize();
+
+            yield return new WaitForSeconds(animationData.AnimationDelay);
+        }
+        _animationView.color = Color.clear;
+
+    }
 }
 
 [System.Serializable]
 public class AnimationData
 {
     public string Name;
-    public Sprite Sprite;
-    public Vector2 ImageCutSetting = new Vector2();
+    public Sprite[] SpriteAnimationArray;
+    public float AnimationDelay = 1.0f;
 }
