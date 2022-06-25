c8e08b77880616ae43c4e50f283c8a8be3af7b06
nogu06 <knocktokotoko@gmail.com>
2022-06-25 17:32:45 +0900

    分割されている2Dエフェクトを再生するスクリプトを書き始めた

---
+++ Assets/Script/EffectAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectAnimation : MonoBehaviour
+{
+    [SerializeField] List<AnimationData> _animationList = new List<AnimationData>();
+}
+
+[System.Serializable]
+public class AnimationData
+{
+    public string Name;
+    public Sprite Sprite;
+    public Vector2 ImageCutSetting = new Vector2();
+}
